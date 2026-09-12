1a1b6ed4d61e2cbe18830380d5dbf763bef23ebe
ejaslar <ejaslar@icloud.com>
2026-09-12 14:43:33 +0200

    main.cpp: Add color support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string>
 
 int main() {
     // open MacBook's built-in camera - id 1 (id 0 is iPhone's continuity camera)
@@ -18,14 +19,12 @@ int main() {
     const std::string ASCII_CHARS = " .:-=+*#%@";
 
     // target width of terminal window
-    const int terminalWidth = 200;
+    const int terminalWidth = 120;
 
-    cv::Mat frame, grayFrame, resizedFrame;
-    std::cout << "Press 'Ctrl + C' to exit." << std::endl;
-
+    cv::Mat frame, grayFrame, resizedGray, resizedColor;
 
     // clear terminal window by ANSI control code
-    std::cout << "\x1b[2j";
+    std::cout << "\x1b[2J";
 
     // loop retrieving frames from camera
     while (true) {
@@ -39,36 +38,52 @@ int main() {
         // make mirror reflection
         cv::flip(frame, frame, 1);
 
-        // convert image to grayscale colorspace
-        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
-
         // calculate new frame ratios (assuming font height to width ratio is 2)
-        float frameRatio = (float)grayFrame.rows / grayFrame.cols;
+        float frameRatio = (float)frame.rows / frame.cols;
         int terminalHeight = terminalWidth * frameRatio * 0.5f;
 
         // change frame size
-        cv::resize(grayFrame, resizedFrame, cv::Size(terminalWidth, terminalHeight));
+        cv::resize(frame, resizedColor, cv::Size(terminalWidth, terminalHeight));
+
+        // convert resized frame to grayscale colorspace
+        cv::cvtColor(resizedColor, resizedGray, cv::COLOR_BGR2GRAY);
 
         // build frame from ascii characters;
         // ANSI code \x1b[H moves cursor to top left corner to override text without flickering
         std::string asciiFrame = "\x1b[H";
 
-        // iterate over pixels of resized frame
-        for (int y = 0; y < resizedFrame.rows; ++y) {
-            for (int x = 0; x < resizedFrame.cols; ++x) {
+        // optimisation: reserve string memory in advance to prevent string resizing in runtime;
+        // 30 is an upper bound of one pixel memory (with safety buffer)
+        asciiFrame.reserve(resizedColor.cols * resizedColor.rows * 30);
+
+        // iterate over pixels of resizedColor frame
+        for (int y = 0; y < resizedColor.rows; ++y) {
+            for (int x = 0; x < resizedColor.cols; ++x) {
+                // retrieve pixel bgr values
+                cv::Vec3b color = resizedColor.at<cv::Vec3b>(y, x);
+                int b = color[0];
+                int g = color[1];
+                int r = color[2];
+
                 // retrieve pixel brightness (0 - black; 255 - white)
-                int pixelBrightness = resizedFrame.at<uchar>(y, x);
+                int pixelBrightness = resizedGray.at<uchar>(y, x);
 
                 // convert pixel into ascii char;
                 // pixelBrightness / 255 is a number form interval [0, 1];
                 // we multiply it by ascii_chars length to choose char
                 // based on this ratio from our char pallete
                 int index = pixelBrightness * (ASCII_CHARS.length() - 1) / 255;
+
+                // build color sequence for given ascii char and add this char to the frame
+                asciiFrame += "\x1b[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
                 asciiFrame += ASCII_CHARS[index];
             }
             // the end of the row - we go to the next one
             asciiFrame += "\n";
         }
+        // reset terminal colors
+        asciiFrame += "\x1b[0m";
+
         std::cout << asciiFrame << std::flush;
     }
 
